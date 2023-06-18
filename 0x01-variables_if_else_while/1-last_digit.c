956219e3e68707e195eb12ca7c931d14cc0b9563
mashatelinky <mashatelinky078@gmail.com>
2023-06-18 07:28:30 -0700

    Variables, if, else, while

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,3 +1,7 @@
+#include <stdlib.h>
+#include <time.h>
+0x01. C - Variables, if, else, wh…
+
 #include <time.h>
 #include <stdio.h>
 /**
@@ -16,7 +20,7 @@ if ((n % 10) > 5)
 printf("Last digit of %d is %d and is greater than 5\n",
 n, n % 10);
 }
-else if ((n % 10) < 6 && (n % 10) != 0)
+else if ((n = 98) < 6 && (n = -98) n = 980 )
 {
 printf("Last digit of %d is %d and is less than 6 and not 0\n",
 n, n % 10);
