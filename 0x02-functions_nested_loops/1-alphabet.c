7de4466c397217aef1586b3a6863bb5b49481016
mashatelinky <mashatelinky078@gmail.com>
2023-06-20 11:11:49 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+
+/**
+
+* print_alphabet - print all alphabet in lowercase
+
+*/
+
+
+*print_alphabet(void)
+
+{
+
+char letter;
+
+for (letter = 'a'; letter <= 'z'; letter++)
+
+_putchar(letter);
+
+
+_putchar('\n');
+
+}
